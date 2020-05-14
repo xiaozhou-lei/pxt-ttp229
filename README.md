95436dc24e684658b3d24129b7fa9aac12d1536c
gchen1996 <1328486271@qq.com>
2020-05-14 16:34:42 +0800

    ttp229.updata

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-ttp229
+# 触摸键盘
+
+触摸键盘积木编程
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+ttp229=github:gchen1996/pxt-ttp229
+```
---
+++ main.ts
@@ -0,0 +1,57 @@
+/*
+TTP229 block
+*/
+  
+//% weight=8 color=#FFD700  block="TTP229"
+namespace ttp229
+{
+
+     //% blockId=ttp229_GetKeyCode
+     //% block="SDOPIN %SDO|SCLPIN %SCL"
+     export function GetKeyCode(SDO: DigitalPin, SCL: DigitalPin): string
+     {
+ 
+          let DATA = 0;
+
+          pins.digitalWritePin(SDO, 1);
+          control.waitMicros(93);
+
+          pins.digitalWritePin(SDO, 0);
+          control.waitMicros(10);
+
+          for (let i = 0; i < 16; i++)
+          {
+
+               pins.digitalWritePin(SCL, 1);
+               pins.digitalWritePin(SCL, 0);
+
+               DATA |= pins.digitalReadPin(SDO) << i;   
+          }
+
+          control.waitMicros(2 * 1000);
+
+          switch (DATA&0xFFFF)
+          {     
+               case 0xFFFE: return "1"; 
+               case 0xFFFD: return "2"; 
+               case 0xFFFB: return "3"; 
+               case 0xFFEF: return "4";  
+               case 0xFFDF: return "5"; 
+               case 0xFFBF: return "6"; 
+               case 0xFEFF: return "7"; 
+               case 0xFDFF: return "8"; 
+               case 0xFBFF: return "9"; 
+               case 0xDFFF: return "0"; 
+               case 0xFFF7: return "A"; 
+               case 0xFF7F: return "B"; 
+               case 0xF7FF: return "C"; 
+               case 0x7FFF: return "D"; 
+               case 0xEFFF: return "*"; 
+               case 0xBFFF: return "#";      
+
+               default: return " ";
+          }          
+     
+     }
+}
+
